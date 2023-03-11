7fea0961d63e0740fed4b0c15c174ab2897c34ad
datvn <datcy2011@gmail.com>
2023-03-11 08:21:44 +0700

    update

--- app/src/main/java/com/ltdd/cringempone/MainActivity.java
+++ app/src/main/java/com/ltdd/cringempone/MainActivity.java
@@ -2,8 +2,12 @@ package com.ltdd.cringempone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+
+import com.ltdd.cringempone.components.MusicPlayer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.i("Sys", "onCreate: ok");
         setContentView(R.layout.activity_main);
+        Button player = findViewById(R.id.btnMusicPlayer);
+        player.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), MusicPlayer.class);
+            startActivity(intent);
+        });
     }
 }
