4aaaf3c40000de766e0c911c42c5cc3ae74e7c9a
Huy Van Pham <87406306+huypv2002@users.noreply.github.com>
2023-03-30 11:05:08 +0700

    Add navigation drawer cho project với những fragment ví dụ. Chuyển activy_main.xml cũ sang activity_main_link.xml

--- app/src/main/java/com/ltdd/cringempone/MainActivity.java
+++ app/src/main/java/com/ltdd/cringempone/MainActivity.java
@@ -4,10 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
@@ -15,19 +25,27 @@ import com.google.gson.JsonParser;
 import com.ltdd.cringempone.api.BaseAPIService;
 import com.ltdd.cringempone.ui.activity.MusicPlayer;
 
+import com.ltdd.cringempone.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
     String TAG = "APP";
     BaseAPIService apiService = BaseAPIService.getInstance();
     Boolean isBound = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_link);
 
         bindAPIBase();
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.appBarMain.toolbar);
 
         //#region Streaming media player
 //        //Set up
@@ -49,12 +67,24 @@ public class MainActivity extends AppCompatActivity {
 //        ImageView img = findViewById(R.id.imgView);
 //        img.setImageDrawable(Helper.LoadImageFromWebOperations("https://photo-resize-zmp3.zmdcdn.me/w165_r1x1_jpeg/cover/8/5/5/b/855bb71b9bc9a577ea6627df65a2adeb.jpg"));
 //
-        Button player = findViewById(R.id.btnMusicPlayer);
-        player.setOnClickListener(view -> {
-//            exoPlayer.stop();
-            Intent intent = new Intent(view.getContext(), MusicPlayer.class);
-            startActivity(intent);
-        });
+//        Button player = findViewById(R.id.btnMusicPlayer);
+//        player.setOnClickListener(view -> {
+////            exoPlayer.stop();
+//            Intent intent = new Intent(view.getContext(), MusicPlayer.class);
+//            startActivity(intent);
+//        });
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
     }
     public void bindAPIBase(){
@@ -104,5 +134,18 @@ public class MainActivity extends AppCompatActivity {
 //        } catch (Exception e) {
 //            Log.e(TAG, "onResume: " + e.getMessage());
 //        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
